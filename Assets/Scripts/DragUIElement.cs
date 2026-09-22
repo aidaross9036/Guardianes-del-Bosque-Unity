d99f481902dd8b795499ce23c7485f1006401d92
aidaross9036 <aida.rosas9036@alumnos.udg.mx>
2026-09-21 19:42:49 -0600

    Agrega detección de colisiones
    Se incorpora detección de colisiones entre el Guardian y los obstáculos de la escena, impidiendo que el personaje los atraviese durante su desplazamiento.

--- Assets/Scripts/DragUIElement.cs
+++ Assets/Scripts/DragUIElement.cs
@@ -6,6 +6,9 @@ public class DragUIElement : MonoBehaviour, IBeginDragHandler, IDragHandler, IEn
     private RectTransform rectTransform;
     private Canvas canvas;
 
+    public RectTransform bearCollision1;
+    public RectTransform bearCollision2;
+
     private void Awake()
     {
         rectTransform = GetComponent<RectTransform>();
@@ -19,7 +22,42 @@ public class DragUIElement : MonoBehaviour, IBeginDragHandler, IDragHandler, IEn
 
     public void OnDrag(PointerEventData eventData)
     {
+        Vector2 previousPosition = rectTransform.anchoredPosition;
+
+        // Intentamos mover al Guardian
         rectTransform.anchoredPosition += eventData.delta / canvas.scaleFactor;
+
+        // Comprobamos si toca alguno de los osos
+        if (IsColliding(bearCollision1) || IsColliding(bearCollision2))
+        {
+            // Si hay colisión, vuelve a la posición anterior
+            rectTransform.anchoredPosition = previousPosition;
+            Debug.Log("El Guardian colisionó con un oso");
+        }
+    }
+
+    private bool IsColliding(RectTransform obstacle)
+    {
+        if (obstacle == null)
+            return false;
+
+        Rect guardianRect = GetWorldRect(rectTransform);
+        Rect obstacleRect = GetWorldRect(obstacle);
+
+        return guardianRect.Overlaps(obstacleRect);
+    }
+
+    private Rect GetWorldRect(RectTransform rt)
+    {
+        Vector3[] corners = new Vector3[4];
+        rt.GetWorldCorners(corners);
+
+        return new Rect(
+            corners[0].x,
+            corners[0].y,
+            corners[2].x - corners[0].x,
+            corners[2].y - corners[0].y
+        );
     }
 
     public void OnEndDrag(PointerEventData eventData)
